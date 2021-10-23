0aaf55839e679f280e70e86a890d77fb8a2103f2
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2021-10-23 22:32:53 +0100

    menu attempt

--- Task/static/task/js/script.js
+++ Task/static/task/js/script.js
@@ -29,3 +29,29 @@ function menuBtnChangecustom() {
    closeBtn.classList.replace("sidebarcustom","sidebar");//replacing the iocns class
  }
 }
+
+
+	/*-----------------------------------------------------*/
+  	/* Navigation Menu
+   ------------------------------------------------------ */  
+  var toggleButton = $('.logo-details'),
+       nav = $('.nav-list');
+
+   // toggle button
+   toggleButton.on('click', function(e) {
+
+		e.preventDefault();
+		toggleButton.toggleClass('is-clicked');
+		nav.slideToggle();
+
+	});
+
+   // nav items
+  	nav.find('li a').on("click", function() {   
+
+   	// update the toggle button 		
+   	toggleButton.toggleClass('is-clicked'); 
+   	// fadeout the navigation panel
+   	nav.fadeOut();   		
+   	     
+  	});
