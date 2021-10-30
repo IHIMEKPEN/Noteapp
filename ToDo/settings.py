c269416baafaf19c59490a990e6bfa38b61ecb48
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2021-10-30 12:01:36 +0100

    Dockerized

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['whilpad-noteapp.herokuapp.com']
+ALLOWED_HOSTS = ['whilpad-noteapp.herokuapp.com','127.0.0.1']
 
 
 # Application definition
